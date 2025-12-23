2afa4293360f6783189a6a24cfc90e4f9c4fd4ad
정승훈 <jsh12345644[@gmail.com](mailto:you@example.com)>
2025-12-23 16:20:11 +0900

    local에서 github보내기 작성

---
+++ main.js
@@ -0,0 +1,9 @@
+// 로컬에서 깃허브로 보내기 위한 연습
+// 1. git init
+// 깃 초기화(github에서 로컬로 clone할땐 필요없음)
+// 2. git remote add origin https://github.com/Nobamb/local_to_github_test.git
+// 목적지에 대한 별칭 지정(레포지토리명 => origin)
+// 3. git branch -M main
+// 브랜치명 변경(local에서 github로 보낼 땐 브랜치명 때문에 충돌날 수 있기에)
+// 4. git push -u origin main
+// origin(목적지(레포지토리)), main(브랜치), -u 기본값 지정
